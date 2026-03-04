c17132b55282f0dcb5942f4fbd41799836245215
HuseyinUtkuYuksel <h.utku64gmail.com>
2026-03-05 00:09:43 +0300

    github connection added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
+import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
@@ -12,6 +13,10 @@ const handler = NextAuth({
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID || "",
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET || "",
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID || "",
+            clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
         }),
         CredentialsProvider({
             name: "Credentials",
